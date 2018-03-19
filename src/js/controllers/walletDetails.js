ca03a8110094113b0759a1d8aaa1b71fa10c0914
Yoyae <gingin.fr@gmail.com>
2018-03-19 09:33:13 +0100

    Fix android color bar

--- src/js/controllers/walletDetails.js
+++ src/js/controllers/walletDetails.js
@@ -424,7 +424,7 @@ angular.module('copayApp.controllers').controller('walletDetailsController', fun
   $scope.$on("$ionicView.afterLeave", function(event, data) {
 
     if ($window.StatusBar) {
-      var statusBarColor = appConfigService.name == 'copay' ? '#192c3a' : '#1e3186';
+      var statusBarColor = '#192c3a';
       $window.StatusBar.backgroundColorByHexString(statusBarColor);
     }
   });
